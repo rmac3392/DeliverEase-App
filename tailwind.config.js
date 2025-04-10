56debbd1b4e70ac8ec6b952e537aeb3be8da60cd
rmac3392 <ryanjamesmacawili@gmail.com>
2025-04-10 21:49:55 +0800

    With QR

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Include all HTML and TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
